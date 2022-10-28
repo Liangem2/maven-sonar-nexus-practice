489414b33900a6632b29bf81a55495327c64a70d
Liangem2 <99054943+Liangem2@users.noreply.github.com>
2022-10-29 00:40:06 +0200

    Revert "bug-fix as per demo-123"

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
-		Integer age = "20";
-		System.out.printIn(age+5);
-		
 	}
 
 }
